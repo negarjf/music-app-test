ff818f7d0790c0ee97147572be078cbfb1d084f7
Negar <n.jamalifard@gmail.com>
2019-12-02 03:15:09 +0330

    feat: add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="header"></div>
+    <div class="header">
+        <div class="header__inner">
+            <div class="logo">
+                <img src="@/assets/images/logo.png" alt="">
+            </div>
+            <h1 class="title">
+                Music Maker
+                <strong>JAM</strong>
+            </h1>
+        </div>
+    </div>
 </template>
 
 <script>
